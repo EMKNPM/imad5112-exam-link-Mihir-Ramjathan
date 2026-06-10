880c2145252a351a89bc8379a08283e8ac5f943c
Mihir <ST10527496@myemeris.edu.za>
2026-06-10 14:28:00 +0200

    this is the detail screens
    the detail screen backend and front end code

--- app/src/main/java/com/example/campsitecommander/DetailActivity.kt
+++ app/src/main/java/com/example/campsitecommander/DetailActivity.kt
@@ -1,6 +1,7 @@
 package com.example.campsitecommander
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -14,11 +15,28 @@ import androidx.core.view.WindowInsetsCompat
 
 class DetailActivity : AppCompatActivity() {
 
+    private lateinit var btnExit: Button
+    private lateinit var tvCategory: Button
+    private lateinit var tvAmount: Button
+    private lateinit var tvComment: Button
+    private lateinit var spinner: Spinner
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_detail)
+
+        spinner = findViewById(R.id.spinner)
+
+
+        btnExit.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
